912288b60c063c713a7f8afc40056f3f9fddf2b9
zuber1077 <zuber1077@yahoo.com>
2018-08-10 11:41:51 +0300

    display heatred store

--- routes/index.js
+++ routes/index.js
@@ -57,6 +57,7 @@ router.post('/account/reset/:token',
 );
 
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 // API
 
